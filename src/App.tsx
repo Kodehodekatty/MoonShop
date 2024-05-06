c8c7f8f851311d4074e753a2d90e6aefe1ce329c
Fredrik Wold <fredrik.wold@bouvet.no>
2024-05-06 18:21:07 +0200

    feat: adds basic item form

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
+import { z } from 'zod';
+import './App.css';
+import { useState } from 'react';
+import { ItemForm } from './components/ItemForm';
 
+const ItemSchema = z.object({
+    name: z.string().min(1),
+    price: z.number().positive(),
+    description: z.string().min(1),
+    id: z.string().uuid(),
+});
 
-import './App.css'
+type Item = z.infer<typeof ItemSchema>;
 
 function App() {
+    const [items, setItems] = useState<Item[]>([]);
 
+    const [cart, setCart] = useState<string[]>([]);
 
-  return (
-    <>
-    <div>test</div>
-    </>
-  )
+    const addItemToCart = (id: string) => {
+        setCart((prev) => [...prev, id]);
+    };
+
+    const addNewItem = (item: Item) => {
+        setItems((prev) => [...prev, item]);
+    };
+
+    return (
+        <div className="container">
+            <ItemForm />
+        </div>
+    );
 }
 
-export default App
+export default App;
